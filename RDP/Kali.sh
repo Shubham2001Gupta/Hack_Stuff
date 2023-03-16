8972d58e9bc9e11d310b7d08784f004a7554a561
Shubham Gupta <89962432+Shubham2001Gupta@users.noreply.github.com>
2023-03-16 19:55:46 +0530

    Update Kali.sh

--- RDP/Kali.sh
+++ RDP/Kali.sh
@@ -32,7 +32,7 @@ echo "===================================="
 echo "Username : ubuntu"
 echo "Password : ubuntu"
 echo "RDP Address:"
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://0.0.0.0:3000/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
 echo "===================================="
 echo "Don't close this tab to keep RDP running"
